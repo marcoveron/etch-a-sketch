dabf1bfa449a350a2aacfa1f5653b592d7675a74
Marco Verón <mv.marcoveron@gmail.com>
2025-12-04 18:30:16 +0800

    Add a button to create a new grid
    a button was added a the top to ask the user for a new size for a new
    grid, after the user input the current grid gets deleted and the new on
    gets created. also a change of the color when a grid is hover was added.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 let container = document.querySelector(".container")
 
+let newGridButton = document.querySelector("button");
+
 for (let i = 0; i<16; i++){
     column = document.createElement("div");
     column.className = 'column';
@@ -10,3 +12,38 @@ for (let i = 0; i<16; i++){
         column.appendChild(row);
     }
 }
+
+let newGridSize = 0;
+
+function insertGrid(size) { 
+
+    if (size >= 100) {
+        size = 100;
+    }
+
+    let columns = document.querySelectorAll(".column");
+    let rows = document.querySelectorAll(".row");
+
+    for (x of columns) {
+        x.remove();
+    }
+
+    for (y of rows) {
+        y.remove();
+    }
+
+    for (let i = 0; i<size; i++){
+        column = document.createElement("div");
+        column.className = 'column';
+        container.appendChild(column);
+        for (let j = 0; j<size; j++){
+            row = document.createElement("div");
+            row.className = 'row';
+            column.appendChild(row);
+        }
+    }
+}
+
+newGridButton.addEventListener("click", () => insertGrid(newGridSize = window.prompt("Please enter the new size of your window")));
+
+
